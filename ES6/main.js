614fde21846f56f7f77d5f5aeef30155a0493f38
EHR-EkramulHaque <ehr.ekramul@gmail.com>
2024-11-21 22:08:41 +0600

    nest project removed

--- ES6/main.js
+++ ES6/main.js
@@ -1,48 +1,42 @@
-// Declaring variables using let and const
-let variable1 = 'Hello'; // let allows you to reassign the variable
-const variable2 = 'World'; // const means the variable cannot be reassigned
+// Variables
 
-// Arrow functions
-const add = (a, b) => {
-    return a + b;
-};
+// let, const
+let name = 'Jhon';
+// let name = 'Alex'; // Error
+name = 'Doe';
+console.log("Let variable: ", name);
 
-// Template literals
-let greeting = `${variable1}, ${variable2}!`; // Embedding variables in strings
+const age = 30;
+// age = 32; // Error
+console.log("Const variable: ", age);
 
-// Destructuring assignment
-const person = {
-    name: 'John',
-    age: 30
-};
+// Block Scope
+let fullName = 'Jhon Doe';
+if (true) {
+    let fullName = 'Alex Doe'; // re-declared. It's a new variable. only available inside block
+    console.log("Inside block fullName re-declared value: ", fullName);
+}
+console.log("Outside block fullName: ", fullName);
 
-const { name, age } = person; // Extracting properties from an object
 
-// Default parameters
-function multiply(a, b = 2) {
-    return a * b;
+let poorCountry = ['Bangladesh', 'Nepal','Bhutan'];
+let richCountry = ['USA', 'UK', 'Canada'];
+
+let allCountry = [...poorCountry, ...richCountry];
+console.log("Marged array using spreed operator(...): ",allCountry);
+
+richCountry.push(poorCountry); // Insert poorCountry array as a single element in richCountry array
+console.log("Marged array using push method: ",richCountry);
+
+
+// Rest parameter
+function calculation(/*a,b,*/...numbers){ // use others parameter before rest. If needed
+    let sum = 0;
+
+    for(let i of numbers){
+        sum += i;
+    }
+    return sum;
 }
 
-// Spread operator
-const arr1 = [1, 2, 3];
-const arr2 = [...arr1, 4, 5, 6]; // Spreading elements of an array
-
-// Classes
-class Animal {
-    constructor(name) {
-        this.name = name;
-    }
-
-    speak() {
-        console.log(`${this.name} makes a noise.`);
-    }
-}
-
-class Dog extends Animal {
-    speak() {
-        console.log(`${this.name} barks.`);
-    }
-}
-
-const dog = new Dog('Rex');
-dog.speak(); // Rex barks.
+console.log("Rest parameter: ", calculation(1,2,3,4,5,6,7,8,9,10));
