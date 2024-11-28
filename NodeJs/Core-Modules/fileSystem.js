c4a11d3a09f3bde06c379852784ba31c8ec7a1a1
EHREkramul <ehr.ekramul@gmail.com>
2024-11-28 20:28:16 +0600

    Update Core-Module Example

--- NodeJs/Core-Modules/fileSystem.js
+++ NodeJs/Core-Modules/fileSystem.js
@@ -3,3 +3,5 @@ const data = fs.readFileSync('testfile.txt', 'utf8'); // Synchronous
 console.log(data);
 
 fs.writeFileSync('output.txt', 'Hello, World!'); // Synchronous
+const data2 = fs.readFileSync('output.txt', 'utf8');
+console.log(data2);
